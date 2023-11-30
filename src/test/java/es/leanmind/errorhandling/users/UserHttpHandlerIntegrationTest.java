7e4c43391106345c10b79c49042010c1a3e42d20
José Rodríguez <jrodalo@gmail.com>
2023-11-30 19:17:35 +0000

    Move test annotations

--- src/test/java/es/leanmind/errorhandling/users/UserHttpHandlerIntegrationTest.java
+++ src/test/java/es/leanmind/errorhandling/users/UserHttpHandlerIntegrationTest.java
@@ -3,8 +3,14 @@ package es.leanmind.errorhandling.users;
 import es.leanmind.errorhandling.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@SpringBootTest
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
 class UserHttpHandlerIntegrationTest extends IntegrationTest {
 
     @Autowired
