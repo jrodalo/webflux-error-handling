f1c9f3aa39b9d81221fb9c224322b33a2216107f
José Rodríguez <jrodalo@gmail.com>
2023-11-30 19:15:06 +0000

    Log errors

--- src/main/java/es/leanmind/errorhandling/errors/ErrorHttpHandler.java
+++ src/main/java/es/leanmind/errorhandling/errors/ErrorHttpHandler.java
@@ -1,5 +1,7 @@
 package es.leanmind.errorhandling.errors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -8,9 +10,11 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class ErrorHttpHandler {
+    private final Logger logger = LoggerFactory.getLogger(ErrorHttpHandler.class);
 
     public Mono<ServerResponse> handle(ServerRequest request, HandlerFunction<ServerResponse> handler) {
         return handler.handle(request)
+            .doOnError(this::logError)
             .onErrorResume(HttpError.class, this::toServerResponse)
             .onErrorResume(Throwable.class, error -> toServerResponse(HttpError.fromUnknown(error)));
     }
@@ -19,5 +23,9 @@ public class ErrorHttpHandler {
         return ServerResponse
             .status(httpError.getHttpStatus())
             .bodyValue(httpError.getBody());
+    }
+
+    private void logError(Throwable error) {
+        logger.error(error.getMessage(), error);
     }
 }
