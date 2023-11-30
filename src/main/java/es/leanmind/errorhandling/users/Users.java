d651ce2b3d96935dd2096c7fb1dcd6cfd0132875
José Rodríguez <jrodalo@gmail.com>
2023-11-30 19:16:50 +0000

    Use a database to store the users (r2dbc-h2)

--- src/main/java/es/leanmind/errorhandling/users/Users.java
+++ src/main/java/es/leanmind/errorhandling/users/Users.java
@@ -1,21 +1,31 @@
 package es.leanmind.errorhandling.users;
 
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.RowMetadata;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 @Repository
 public class Users {
-    private final Map<String, User> database = new ConcurrentHashMap<>(
-        Map.of(
-            "1", new User("1", "First User"),
-            "2", new User("2", "Second User")
-        )
-    );
+    private final DatabaseClient databaseClient;
+
+    public Users(DatabaseClient databaseClient) {
+        this.databaseClient = databaseClient;
+    }
 
     public Mono<User> findById(String userId) {
-        return Mono.justOrEmpty(database.get(userId));
+        return databaseClient
+            .sql("SELECT id, name FROM users WHERE id = :userId")
+            .bind("userId", userId)
+            .map(this::toUser)
+            .one();
+    }
+
+    private User toUser(Row row, RowMetadata rowMetadata) {
+        return new User(
+            row.get("id", String.class),
+            row.get("name", String.class)
+        );
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (
+    id varchar2 PRIMARY KEY,
+    name varchar2
+);
